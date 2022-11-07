1e88d36fe524505904b7c0752139969037a57ce1
Serhii Mashentsev <d.ashesss@gmail.com>
2022-11-08 00:45:14 +0200

    implement category service store

---
+++ internal/categories/integration_test.go
@@ -0,0 +1,50 @@
+package categories_test
+
+import (
+	"fmt"
+	"github.com/d-ashesss/mah-moneh/internal/categories"
+	"github.com/stretchr/testify/suite"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"os"
+	"testing"
+)
+
+type CategoriesIntegrationTestSuite struct {
+	suite.Suite
+	db  *gorm.DB
+	srv *categories.Service
+}
+
+func (ts *CategoriesIntegrationTestSuite) SetupSuite() {
+	dbHost := os.Getenv("POSTGRES_HOST")
+	dbPort := os.Getenv("POSTGRES_PORT")
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPwd := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	if dbHost == "" {
+		ts.T().Skip("No DB configuration provided.")
+	}
+	dsn := fmt.Sprintf("host=%s user=%s password=%s database=%s", dbHost, dbUser, dbPwd, dbName)
+	if dbPort != "" {
+		dsn = fmt.Sprintf("%s port=%s", dsn, dbPort)
+	}
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Discard})
+	if err != nil {
+		ts.T().Fatalf("Failed to connect to the DB: %s", err)
+	}
+	ts.db = db
+	store := categories.NewGormStore(db)
+	ts.srv = categories.NewService(store)
+}
+
+func (ts *CategoriesIntegrationTestSuite) TestSaveCategory() {
+}
+
+func (ts *CategoriesIntegrationTestSuite) TestDeleteCategory() {
+}
+
+func TestCategoriesIntegration(t *testing.T) {
+	suite.Run(t, new(CategoriesIntegrationTestSuite))
+}
--- internal/categories/store.go
+++ internal/categories/store.go
@@ -1,8 +1,29 @@
 package categories
 
-import "context"
+import (
+	"context"
+	"gorm.io/gorm"
+)
 
 type Store interface {
 	SaveCategory(ctx context.Context, cat *Category) error
 	DeleteCategory(ctx context.Context, cat *Category) error
+}
+
+type gormStore struct {
+	db *gorm.DB
+}
+
+func NewGormStore(db *gorm.DB) Store {
+	return &gormStore{db: db}
+}
+
+func (g *gormStore) SaveCategory(ctx context.Context, cat *Category) error {
+	//TODO implement me
+	panic("implement SaveCategory")
+}
+
+func (g *gormStore) DeleteCategory(ctx context.Context, cat *Category) error {
+	//TODO implement me
+	panic("implement DeleteCategory")
 }
