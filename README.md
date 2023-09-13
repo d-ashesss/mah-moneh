cc558b90c65f18d4b1cbcdd59535112f74dbb676
Serhii Mashentsev <serhii@mashentsev.me>
2023-09-13 20:36:42 +0300

    get JWKs URL from OpenID configuration URL

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Mah Moneh
 
-This piece of software is supposed to help to somehow observe and manage your own finances 🤷
-
 [![test status](https://github.com/d-ashesss/mah-moneh/workflows/test/badge.svg?branch=main)](https://github.com/d-ashesss/mah-moneh/actions)
 [![Go Report Card](https://goreportcard.com/badge/github.com/d-ashesss/mah-moneh)](https://goreportcard.com/report/github.com/d-ashesss/mah-moneh)
-[![MIT license](https://img.shields.io/github/license/d-ashesss/mah-moneh?color=blue)](https://opensource.org/licenses/MIT)
 [![Go version](https://img.shields.io/github/go-mod/go-version/d-ashesss/mah-moneh)](https://github.com/d-ashesss/mah-moneh/blob/main/go.mod)
 [![latest tag](https://img.shields.io/github/v/tag/d-ashesss/mah-moneh?include_prereleases&sort=semver)](https://github.com/d-ashesss/mah-moneh/tags)
+[![MIT license](https://img.shields.io/github/license/d-ashesss/mah-moneh?color=blue)](https://opensource.org/licenses/MIT)
 [![feline reference](https://img.shields.io/badge/may%20contain%20cat%20fur-%F0%9F%90%88-blueviolet)](https://github.com/d-ashesss/mah-moneh)
+
+This piece of software is supposed to help to somehow observe and manage your own finances 🤷
 
 ## Configuration
 
@@ -20,3 +20,4 @@ The configuration is done via environment variables. The following variables are
 * `DB_USER` - database user, default: postgres
 * `DB_PASSWORD` - database user's password, required
 * `DB_DEBUG` - whether to use the database in debug mode
+* `AUTH_OPENID_CONFIGURATION_URL` - OpenID configuration URL for OIDC provider.
--- internal/auth/service.go
+++ internal/auth/service.go
@@ -2,16 +2,24 @@ package auth
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/d-ashesss/mah-moneh/internal/users"
 	"github.com/gofrs/uuid"
 	"github.com/lestrrat-go/jwx/v2/jwk"
 	"github.com/lestrrat-go/jwx/v2/jwt"
+	"io"
 	"log"
+	"net/http"
+	"time"
 )
 
 type UsersService interface {
 	GetUser(ctx context.Context, UUID uuid.UUID) (*users.User, error)
+}
+
+type openidConfiguration struct {
+	JwksUri string `json:"jwks_uri"`
 }
 
 type Service struct {
@@ -22,17 +30,46 @@ type Service struct {
 func NewService(cfg *Config, usersSrv UsersService) *Service {
 	set := jwk.NewSet()
 	if cfg.OpenIDConfigurationUrl != "" {
-		log.Printf("[AUTH] Fetching keys from %s", cfg.OpenIDConfigurationUrl)
-		var err error
-		set, err = jwk.Fetch(context.Background(), cfg.OpenIDConfigurationUrl)
+		openidCfg, err := fetchOpenIDConf(cfg.OpenIDConfigurationUrl)
 		if err != nil {
 			panic(err)
+		}
+
+		if openidCfg.JwksUri != "" {
+			log.Printf("[AUTH] Fetching keys from %s", openidCfg.JwksUri)
+			set, err = jwk.Fetch(context.Background(), openidCfg.JwksUri)
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
 	return &Service{
 		keySet: set,
 		users:  usersSrv,
 	}
+}
+
+func fetchOpenIDConf(url string) (*openidConfiguration, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	buf, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	var cfg openidConfiguration
+	if err := json.Unmarshal(buf, &cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
 }
 
 func (s *Service) AddKey(k jwk.Key) error {
